e6ee7e45bae1fcd34855e8e7daaee44af01ed572
Adam Gosztolai <a.gosztolai@gmail.com>
2019-12-10 18:54:36 +0100

    .

--- benchmark/run_benchmark_LFR.py
+++ benchmark/run_benchmark_LFR.py
@@ -3,9 +3,8 @@ import sys as sys
 sys.path.append('../utils')
 import os as os
 import yaml as yaml
-from geometric_clustering import Geometric_Clustering
-from graph_generator import generate_graph
-from misc import save_curvature, save_clustering, load_curvature
+from geometric_clustering.geometric_clustering import Geometric_Clustering 
+from geometric_clustering.utils import misc 
 import numpy as np
 
 #Set parameters
@@ -23,19 +22,18 @@ if postprocess != 1:
     # =============================================================================
     # Main loop: repeat for all parameters and network realisations
     # =============================================================================
-    for i in mu:
-        os.chdir('/data/AG/geocluster/LFR/mu' + str(mu))
+    for _,i in enumerate(mu):
+        os.chdir('/data/AG/geocluster/LFR/mu' + str(i))
         
         for k in range(numGraphs):    
-            params['filename'] = 'graph_'+str(k)+'_mu_'+str(mu[i])
-            params['mu'] = mu[i]
+            params['filename'] = 'LFR_'+str(k)+'_'
                    
-            G = nx.read_gpickle(fname + ".gpickle")
-            G.graph['name'] = 'graph_'+str(k)+'_mu_'+str(mu[i])
+            G = nx.read_gpickle(params['filename'] + ".gpickle")
+            G.graph['name'] = 'graph_'+str(k)+'_mu_'+str()
             
             # initialise the code with parameters and graph
             T = np.logspace(params['t_min'], params['t_max'], params['n_t'])
-            gc = Geometric_Clustering(G, T=T, workers=workers, cutoff=0.99, workers=workers, GPU=True, lamb=0.1)
+            gc = Geometric_Clustering(G, T=T, workers=workers, cutoff=0.99, GPU=True, lamb=0.1)
 
             gc.compute_OR_curvatures()
             misc.save_curvature(gc)
