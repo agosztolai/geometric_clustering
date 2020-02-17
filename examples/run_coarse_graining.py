e9be10aa02c9849498dd2dfb8f24f3f30d87fb33
alexis arnaudon <alexis.arnaudon@epfl.ch>
2020-02-17 16:46:04 +0100

    coarse graining example script

---
+++ examples/run_coarse_graining.py
@@ -0,0 +1,31 @@
+import sys as sys
+import os
+import numpy as np
+import yaml
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+import geocluster as gc 
+from geocluster import plotting, io
+from graph_library import generate
+
+#get the graph from terminal input 
+whichgraph = sys.argv[-1]     
+
+#load parameters
+paramsfile='graph_params.yaml'
+params = yaml.load(open(paramsfile,'rb'), Loader=yaml.FullLoader)[whichgraph]
+
+os.chdir(whichgraph)
+
+#Load graph 
+graph = generate(whichgraph=whichgraph, params=params)
+         
+#Compute the OR curvatures
+times, kappas = io.load_curvature()
+
+edge_scales = gc.compute_scales(times, kappas)
+
+graphs_reduc = gc.coarse_grain(graph, edge_scales, times[:20])
+plotting.plot_coarse_grain(graphs_reduc, node_size=20, edge_width=1)
