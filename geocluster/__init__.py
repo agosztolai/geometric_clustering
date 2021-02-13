cecb65a6b93a9008bb8e438a2c435618740a631b
alexis arnaudon <alexis.arnaudon@epfl.ch>
2021-02-13 10:42:20 +0100

    init update

--- geocluster/__init__.py
+++ geocluster/__init__.py
@@ -1,5 +1,5 @@
 """Import main functions."""
-from .clustering import cluster_signed_modularity
-from .curvature import compute_curvatures
-from .curvature import compute_OR_curvature
-from .io import load_curvature
+from geocluster.clustering import cluster_signed_modularity
+from geocluster.curvature import compute_curvatures
+from geocluster.curvature import compute_OR_curvature
+from geocluster.io import load_curvature
